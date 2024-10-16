3b97de83e4a91e387df62e842c1a7e0ffce4cd9b
swathi <swathibalasubramanyam@gmail.com>
2024-10-16 12:21:41 -0700

    updates to currency converter

--- currency_converter.py
+++ currency_converter.py
@@ -18,11 +18,28 @@ def setup():
     if not response['quotas']['month']['remaining']:
         raise Exception("API quota over for the month, retry later")
 
+def get_conversion_rates(curr_code):
+    res = requests.get(f"{BASE_URL}latest?apikey={API_KEY}&currencies={curr_code}")
+    print(f"One USD is approximately equivalent to {res.json()['data'][curr_code]} {curr_code}")
+
 def get_currencies():
     response = requests.get(f"{BASE_URL}currencies?apikey={API_KEY}")
 
-    for key, val in response.json()['data'].items():
+    currency_data = response.json()['data']
+
+    for key, val in currency_data.items():
         print(key, val["symbol"], val["name"])
+
+    while True:
+        base_currency = input("Please enter the base currency for which you want to get the conversion rate or type Q to quit: ")
+
+        if base_currency in ["Q", "q"]:
+            return
+
+        if base_currency not in currency_data:
+            continue
+
+        get_conversion_rates(base_currency)
 
 
 def main():
